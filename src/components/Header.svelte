988ed69ab682c773392c414dd089229edf5bc26a
Brian Ginsburg <gins@brianginsburg.com>
2023-04-03 13:17:15 -0700

    Add Fission publishing

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,10 +3,15 @@
 </script>
 
 <div
-  class="absolute z-10 top-0 grid grid-cols-[1fr_auto] content-center w-full h-12 px-3 py-2 bg-white/50 backdrop-opacity-100"
+  class="absolute z-10 top-0 grid grid-cols-[1fr_auto] content-center w-full h-12 px-3 py-2 bg-black/95 backdrop-opacity-100"
 >
-  <span class="text-xl font-[500]">IPVM Homestar Demo</span>
-  <a href="https://github.com/ipvm-wg/homestar" target="__blank" rel="noreferrer">
+  <span class="text-xl text-white font-[500]">IPVM Homestar Demo</span>
+  <a
+    class="text-white"
+    href="https://github.com/ipvm-wg/homestar"
+    target="__blank"
+    rel="noreferrer"
+  >
     <GitHubIcon />
   </a>
 </div>
