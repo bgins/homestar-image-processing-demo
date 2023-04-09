dea847e5e1c9b0013cce747300afb78170c92695
Brian Ginsburg <gins@brianginsburg.com>
2023-04-09 09:12:18 -0700

    Add task icon animations

--- src/components/icons/CheckCircle.svelte
+++ src/components/icons/CheckCircle.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+  import { draw } from 'svelte/transition'
+  import { quartIn } from 'svelte/easing'
+</script>
+
 <svg
   xmlns="http://www.w3.org/2000/svg"
   width="16"
@@ -11,5 +16,5 @@
   class="feather feather-check-circle"
 >
   <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-  <polyline points="22 4 12 14.01 9 11.01" />
+  <polyline in:draw={{ delay: 0, duration: 100, easing: quartIn }} points="9 11.01 12 14.01 22 4" />
 </svg>
--- src/components/icons/MinusCircle.svelte
+++ src/components/icons/MinusCircle.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+  import { draw } from 'svelte/transition'
+  import { quartIn } from 'svelte/easing'
+</script>
+
 <svg
   xmlns="http://www.w3.org/2000/svg"
   width="16"
@@ -9,5 +14,7 @@
   stroke-linecap="round"
   stroke-linejoin="round"
   class="feather feather-minus-circle"
-  ><circle cx="12" cy="12" r="10" /><line x1="8" y1="12" x2="16" y2="12" /></svg
 >
+  <circle cx="12" cy="12" r="10" />
+  <line in:draw={{ delay: 0, duration: 125, easing: quartIn }} x1="8" y1="12" x2="16" y2="12" />
+</svg>
